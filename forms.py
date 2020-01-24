4139747afaadfe5011e78aeae84c3786991185e4
zgf2022 <zgf2022@gmail.com>
2020-01-24 15:16:00 -0600

    re-add settings page

---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField,HiddenField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RoomForm(FlaskForm):
+    campusname = SelectField('Campus', choices=[('admin', 'admin'), ('khs', 'khs'), ('kms', 'kms'),
+    	('kis', 'kis'), ('kes', 'kes'), ('kps', 'kps')], validators=[DataRequired()])
+    primaryuse = SelectField('Primary users',choices=[('cte', 'cte'), ('sped', 'sped'), ('staff', 'staff'), ('generaled', 'generaled')], validators=[DataRequired()])
+    roomname = StringField('Room Name', validators=[DataRequired()])
+    submit = SubmitField('Add Room')
+
+class ItemForm(FlaskForm):
+	itemtype = SelectField('Type', validators=[DataRequired()])
+	itemcondition = SelectField('Condition', choices=[('4','4'), ('3','3'), ('2','2'), ('1','1')], validators=[DataRequired()])
+	itemnote = StringField('Note')
+	itemquantity = StringField('Quantity - leave blank for 1')
+	roomid = HiddenField()
+	submit = SubmitField('Add Item')
+
+class SettingsForm(FlaskForm):
+	appname = StringField('Directory Title')
+	submit = SubmitField('Save Changes')
--- intradirectory.py
+++ intradirectory.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from forms import RoomForm, ItemForm, SettingsForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '57e8728bb0b13ce0c676dfde280ba245'
@@ -21,5 +21,14 @@ class listing(db.Model):
 def home():
     return render_template('main.html', config=config)
 
+@app.route("/settings", methods=['GET', 'POST'])
+def settings():
+	form = SettingsForm()
+	if form.validate_on_submit():
+		config["appname"] = form.appname.data
+		flash('Settings Saved', 'success')
+		return redirect('/settings')
+	return render_template('settings.html', title='Settings',
+								form=form, legend='Settings', config=config)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
